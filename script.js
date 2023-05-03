f99bec6e1e714b4d59d5a1f06c93ba34acf75d4c
Jaider <jaidercueto.1010@gmail.com>
2023-05-03 18:55:28 -0500

    Se hace el llamado a api y se crea un div por cada elemento en el array

---
+++ script.js
@@ -0,0 +1,35 @@
+const API_MAIN = 'https://api.jikan.moe/v4/anime?page=1&limit=20'
+
+const mainEl = document.getElementById('main')
+
+getAnimes()
+
+async function getAnimes() {
+    const response = await fetch(API_MAIN)
+    const res = await response.json()
+    
+    showAnimes(res.data)
+}
+
+function showAnimes(animesArray) {
+    mainEl.innerHTML = ''
+
+    animesArray.forEach(anime => {
+        const {title, images, score, genres} = anime
+
+        const animeEl = document.createElement('div')
+        animeEl.classList.add('animeDiv')
+
+        animeEl.innerHTML = `
+            <h4>${title}</h4>
+            <img src=${images.jpg.image_url} alt=${title}/>
+            <h4>${score}</h4>
+            <div>
+                ${genres.map(genre => {
+                    return `<h5>${genre.name}</h5>`
+                })}
+            </div>
+        `
+        mainEl.appendChild(animeEl)
+    });
+}
