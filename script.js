414ee752ad1533bcc2fc62b02607c51956649ad0
Jaider <jaidercueto.1010@gmail.com>
2023-05-09 18:44:06 -0500

    animeDiv element changes, api response changed to 9 elements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_MAIN = 'https://api.jikan.moe/v4/anime?page=1&limit=20'
+const API_MAIN = 'https://api.jikan.moe/v4/anime?page=1&limit=9'
 
 const mainEl = document.getElementById('main')
 
@@ -21,13 +21,13 @@ function showAnimes(animesArray) {
         animeEl.classList.add('animeDiv')
 
         animeEl.innerHTML = `
-            <h4>${title}</h4>
+            <h3>${title}</h3>
             <img src=${images.jpg.image_url} alt=${title}/>
-            <h4>${score}</h4>
+            <h4>${score} ★</h4>
             <div>
                 ${genres.map(genre => {
-                    return `<h5>${genre.name}</h5>`
-                })}
+                    return `<h5> • ${genre.name}</h5>`
+                }).join('')}
             </div>
         `
         mainEl.appendChild(animeEl)
